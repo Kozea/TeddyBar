622ab3dd52c57f0fe3281a30cc6d69833cc0e647
Jérémy James <jeremy.james@kozea.fr>
2012-03-22 14:39:57 +0100

    Update teddybar config and rm useless icons

--- teddybar.js
+++ teddybar.js
@@ -1,45 +1,77 @@
 (function ($) {
 	$.fn.teddybar = function (options) {
-		var settings = {
-			teddybar_url : "teddybar/teddybar.html",
+		var config = {
 			document : document, // if iframe use : document.getElementById('iframe').contentWindow.document
-			save : null,
-			generate : null,
-			upload : null,
-			history : null
-        };
-        if (options) {
-            $.extend(settings, options);
-        }
+			menu : {
+				'Bold': 'bold',
+				'Italic': 'italic',
+				'Underline': 'underline',
+				'Strikethrough': 'strikethrough',
+				'-1': null,
+			  'Font Name': ['fontname', {'Fontin': 'fontin', 'Latin': 'latin modern'}],
+				'Font Size': ['fontsize', {'1 (8pt)': '1', '2 (10pt)': '2','3 (12pt)': '3','4 (14pt)': '4','5 (18pt)': '5','6 (24pt)': '6','7 (36pt)': '7'}],
+				'Format': ['formatblock', {'Heading 1': 'h1','Heading 2': 'h2','Heading 3': 'h3','Heading 4': 'h4','Heading 5': 'h5','Heading 6': 'h6','Paragraph': 'p','Preformatted': 'pre'}],
+				'-2': null,
+				'Numbered List': 'insertorderedlist',
+				'Unnumbered List': 'insertunorderedlist',
+				'-3': null,
+				'Justify Left': 'justifyleft',
+				'Justify Right': 'justifyright',
+				'Center': 'justifycenter',
+				'Justify Full': 'justifyfull',		
+				'-4': null,
+				'Undo': 'undo',
+				'Redo': 'redo',			
+				'-5': null,
+				'Save': 'save'
+			},
+			commands : {
+				'save': null
+			}
+		};
+    if (options) {
+        $.extend(config.commands, options);
+    }
 		return this.each(function () {
-			$(this).load(settings.teddybar_url, function () {
-				// Puts the toolbar above the body
-				$('body').css({'margin-top': $(this).height()});
-		
+			var $this=$(this);
+				var block = $('<ul>').appendTo($this);
+				$.each(config.menu, function (name, command) {
+					if (command == null) {
+						// make a separator
+						block = $('<ul>').appendTo($this);
+					} else if (typeof command == 'string') {
+						// make a button
+						$('<input>', {"title": name, "type": "button", "value": name, "class": command, "data-command": command}).appendTo(block).wrap('<li>');
+					} else {
+						// make a select
+						$('<select>', {"title": name, "data-command": command[0]}).appendTo(block).wrap('<li>');
+						$.each(command[1], function(label, value) {
+							$('select[data-command='+command[0]+']').append($('<option>', {"value":value}).html(label));
+						});
+					}
+				});
+				
 				// execCommand on select elements
 				$('select').on('click','select option', function() {
 					var command = $(this).parent().attr('data-command');
 					var value = $(this).val();
-					settings.document.execCommand(command, false, value);
-				        
-					// Font element is deprecated. transform the size attr to style attr with fontsize
-					if(command === 'fontsize') {
-						var sizes = {'1':'8pt','2':'10pt','3':'12pt','4':'14pt','5':'18pt','6':'24pt','7':'36pt'};
-						$(settings.document).find('font').css({'font-size': Sizes[value]});
-					}
+					config.document.execCommand(command, false, value);
 				});
+
 				// execCommand on input elements
 				$('input[type=button]').on('click', function () {
 					var command = $(this).attr('data-command');
-					settings.document.execCommand(command,false,'');
+					if (command in config.commands) {
+						config.commands[command]();
+					} else {
+						config.document.execCommand(command,false,'');
+					}
 				});
-				
-				// Customized functions
-				$('.save').on('click', function () { settings.save() });
-				$('.generate').on('click', function () { settings.generate() });
-				$('.upload').on('click', function () { settings.upload() });
-				$('.history').on('click', function () { settings.history() });
-			});
+
+				// function that always puts the toolbar above the body
+				$(window).resize(function() {
+					$('body').css('margin-top',$this.height());
+				});
 		});
 	};
 })(jQuery);
